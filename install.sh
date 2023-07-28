73eb5321935fbee64ff03284633e2f0589850346
Konstantin Lipinskiy <kastentop2005@gmail.com>
2023-07-28 22:35:48 +0300

    install.sh: refactor

--- install.sh
+++ install.sh
@@ -1,40 +1,57 @@
 #!/usr/bin/bash
 
-servers=("google.com" "raw.githubusercontent.com")
+# Colors
+normal='\033[0m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[1;33m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+lightgray='\033[0;37m'
+darkgray='\033[1;30m'
+lightred='\033[1;31m'
+lightgreen='\033[1;32m'
+lightblue='\033[1;34m'
+lightpurple='\033[1;35m'
+lightcyan='\033[1;36m'
+white='\033[1;37m'
 
-for server in "${servers[@]}"; do
-    if ! ping -c 1 -W 3 "$server"&> /dev/null; then
-        echo -e "\e[1;31m$server is not reachable with your current network.\nChange your network configuration\e[0m"
-    fi
-done
-
-if [ -z "$TERMUX_VERSION" ]; then
-    echo -e "\e[1;31mTermux hasn't been detected!\e[0m\n\e[1;31mInstallation has been aborted!\e[0m"
+# Check for working internet connection
+wget -q --spider http://google.com
+if [ ! $? -eq 0 ]; then
+    printf "${red}Can't connect to the internet! Aborting..."
     exit 1
 fi
 
+# Check if we are running from Termux
+if [ -z "$TERMUX_VERSION" ]; then
+    printf "${red}Termux hasn't been detected. Are you sure you have installed and are running it?${normal}"
+    exit 1
+fi
+
+# Check for Revancify X installation
 if [ -d "$HOME/RevancifyX" ]; then
-    ./RevancifyX/revx
+    printf "${lightblue}Revancify X is already installed, run it with \'revx\' command${normal}"
     exit 0
 fi
 
 if ! command -v git &> /dev/null; then
     if ! pkg update -y -o Dpkg::Options::="--force-confnew"; then
-        echo -e "\e[1;31mOops !!
-Possible causes of error:
-1. Termux from Play Store is not maintained. Download Termux from GitHub.
-2. Unstable internet Connection.
-3. Repository issues. Clear Termux Data and retry."
+        printf
+        "${red}Couldn't update packages. Make sure you have installed Termux from GitHub. Otherwise, your connection might be unstable${normal}"
         exit 1
     fi
     pkg install git -y -o Dpkg::Options::="--force-confnew"
 fi
 
-if git clone --depth=1 https://github.com/kastentop2005/Revancify-X.git; then
+# Download Revancify X
+if git clone --depth=1 https://github.com/kastentop2005/Revancify-X; then
     mv Revancify-X/ RevancifyX/
-    ./RevancifyX/revx
+    printf "${green}Revancify X has been successfully installed!${normal}
+${lightblue}Run it with \'revx\' command${normal}\n"
 else
-    echo -e "\e[1;31mInstallation failed!\e[0m"
-    echo "Please try again"
+    echo "${red}Couldn't download required resources. Can't proceed${normal}"
     exit 1
 fi
